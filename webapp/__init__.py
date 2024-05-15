1ea7a7868a7fd4c85823f3ad3aa16c8473bf69a5
Niek <niekwestra02@gmail.com>
2024-05-15 11:50:42 +0200

    Fix login issue and update navbar routes

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from webapp.main.forms import InfoForm
 
 
 app = Flask(__name__)
@@ -15,12 +14,16 @@ login_manager = LoginManager(app)
 login_manager.init_app(app)
 
 from webapp.main.views import main
+
 app.register_blueprint(main)
+
 
 @login_manager.user_loader
 def load_user(user_id):
     from webapp.models import User
+
     return User.query.get(int(user_id))
+
 
 # Voer db.create_all() uit om de database te maken
 with app.app_context():
